6ce631d58b5fcb8752f2c61c53df46207f82fe8a
Liam Raper <liamwilson127@gmail.com>
2018-04-09 22:34:10 +0100

    Added missing model for login Added missing style for login page Fixed transform issue with user controller

--- Three-Layered Website Plus/WebLayer/Controllers/UserController.cs
+++ Three-Layered Website Plus/WebLayer/Controllers/UserController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Security;
-using WebSite34.Web.Models.User;
+using $safeprojectname$.Models.User;
 
-namespace WebSite34.Web.Controllers
+namespace $safeprojectname$.Controllers
 {
     public class UserController : Controller
     {
---
+++ Three-Layered Website Plus/WebLayer/Models/User/Login.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Security;
+
+namespace $safeprojectname$.Models.User
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "No username was provided.")]
+        [MaxLength(30,ErrorMessage = "Username is to long.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "No password was provided.")]
+        public string Password { get; set; }
+
+        public bool KeepLoggedIn { get; set; }
+
+    }
+}
