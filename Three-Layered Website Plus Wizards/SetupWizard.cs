eaae6afa1194b419840d3609a60806e17cbbf63e
Liam Raper <liamwilson127@gmail.com>
2018-04-10 23:01:51 +0100

    Added project for wizards and added setup wizard class which opens an empty windows form for now

---
+++ Three-Layered Website Plus Wizards/SetupWizard.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+
+namespace Three_Layered_Website_Plus_Wizards
+{
+    class SetupWizard : IWizard
+    {
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            var setupWiz = new SetupWizardForm();
+            setupWiz.ShowDialog();
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+            //This is not called
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            //This is not called
+            return true;
+        }
+
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+            //This is not used
+        }
+
+        public void RunFinished()
+        {
+
+        }
+    }
+}
