97984f835f2ac7ed61202bbf04f462618b0e3d09
Liam Raper <liamwilson127@gmail.com>
2018-04-10 23:01:51 +0100

    #1 Added project for wizards and added setup wizard class which opens an empty windows form for now

---
+++ Three-Layered Website Plus Wizards/SetupWizardForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Three_Layered_Website_Plus_Wizards
+{
+    public partial class SetupWizardForm : Form
+    {
+        public SetupWizardForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
